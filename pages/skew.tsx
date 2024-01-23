128b56f9b35213ca4355417178911714facdc89d
Akshat Mittal <akshatmittal2506@gmail.com>
2024-01-23 21:08:46 +0530

    feat: added skew page

---
+++ pages/skew.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SkewPage: React.FC = () => {
+    return <main className="skew-page w-screen h-screen flex justify-center items-center">
+        <div className="tv-screen">
+            <div className="tv-screen-inner"></div>
+        </div>
+    </main>
+};
+
+export default SkewPage;
