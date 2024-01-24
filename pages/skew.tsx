a73ff2c0463a1f1a1f252f1f326b742a0290f17d
Akshat Mittal <akshatmittal2506@gmail.com>
2024-01-24 16:54:22 +0530

    added plain boxes

--- pages/skew.tsx
+++ pages/skew.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 const SkewPage: React.FC = () => {
-    return <main className="skew-page w-screen h-screen flex justify-center items-center">
-        <div className="tv-screen">
-            <div className="tv-screen-inner"></div>
-        </div>
-    </main>
+	return (
+		<main className="skew-page w-screen h-screen flex justify-center items-center">
+			<div className="tv-screen">
+				<div className="tv-screen-inner"></div>
+            </div>
+            <div className="box box-1">Akshat X</div>
+            <div className="box box-2">Akshat Y</div>
+            <div className="box box-3">Akshat Z</div>
+		</main>
+	);
 };
 
 export default SkewPage;
